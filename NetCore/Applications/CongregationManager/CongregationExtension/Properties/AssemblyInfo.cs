10b5aa15545e57de47d234e8441b82b44b22dd28
gdosborne <greg@gdosborne.com>
2023-01-27 12:14:40 -0600

    Add files via upload

---
+++ NetCore/Applications/CongregationManager/CongregationExtension/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+using Common.Applicationn.Reflection;
+
+[assembly: AssemblyTitle("Properties")]
+[assembly: AssemblyDescription("Not set")]
+[assembly: Guid("00EE81AE-E951-4509-8C0F-BDB59DAD6B01")]
+
+[assembly: AssemblyVersion("2023.1.24.0")]
+[assembly: AssemblyFileVersion("2023.1.24.0")]
+[assembly: AssemblyInformationalVersion("2023.1.24.0")]
+
+[assembly: ApplicationType(ApplicationTypeAttribute.ApplicationTypes.ClassLibrary)]
+[assembly: ApplicationLastBuildDate("01/24/2023")]
+[assembly: VersionSchemaName("daily")]
