91c7e73cf555b3398b9656459b1fd0d93d81914f
gdosborne <greg@gdosborne.com>
2023-01-27 12:16:10 -0600

    Add files via upload

---
+++ NetCore/Applications/CongregationManager/CongregationExtension/ViewModels/CongregationWindowViewModel.cs
@@ -0,0 +1,64 @@
+﻿using Common.Applicationn;
+using Common.MVVMFramework;
+using CongregationManager.Data;
+
+namespace CongregationExtension.ViewModels {
+    public class CongregationWindowViewModel : ViewModelBase {
+        public CongregationWindowViewModel() {
+            Title = "Congregation [design]";
+        }
+
+        public override void Initialize() {
+            base.Initialize();
+
+            Title = "Congregation";
+        }
+
+        public enum Actions {
+            CloseWindow,
+            AcceptData
+        }
+
+        public Settings AppSettings { get; set; }
+        public DataManager DataManager { get; set; }
+
+        #region Name Property
+        private string _Name = default;
+        /// <summary>Gets/sets the Name.</summary>
+        /// <value>The Name.</value>
+        public string Name {
+            get => _Name;
+            set {
+                _Name = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+
+        #region CloseWindowCommand
+        private DelegateCommand _CloseWindowCommand = default;
+        /// <summary>Gets the CloseWindow command.</summary>
+        /// <value>The CloseWindow command.</value>
+        public DelegateCommand CloseWindowCommand => _CloseWindowCommand ?? (_CloseWindowCommand = new DelegateCommand(CloseWindow, ValidateCloseWindowState));
+        private bool ValidateCloseWindowState(object state) => true;
+        private void CloseWindow(object state) {
+            ExecuteAction(nameof(Actions.CloseWindow));
+        }
+        #endregion
+
+        #region AcceptDataCommand
+        private DelegateCommand _AcceptDataCommand = default;
+        /// <summary>Gets the AcceptData command.</summary>
+        /// <value>The AcceptData command.</value>
+        public DelegateCommand AcceptDataCommand => _AcceptDataCommand ?? (_AcceptDataCommand = new DelegateCommand(AcceptData, ValidateAcceptDataState));
+        private bool ValidateAcceptDataState(object state) {
+            var result = true;
+            result &= !string.IsNullOrEmpty(Name);
+            return result;
+        }
+        private void AcceptData(object state) {
+            ExecuteAction(nameof(Actions.AcceptData));
+        }
+        #endregion
+    }
+}
---
+++ NetCore/Applications/CongregationManager/CongregationExtension/ViewModels/ExtensionControlViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Common.MVVMFramework;
+using CongregationManager.Data;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CongregationExtension.ViewModels {
+    public class ExtensionControlViewModel : ViewModelBase {
+        public ExtensionControlViewModel() {
+
+        }
+
+        public override void Initialize() {
+            base.Initialize();
+
+            Congregations = new ObservableCollection<Congregation>();
+        }
+
+        #region Congregations Property
+        private ObservableCollection<Congregation> _Congregations = default;
+        /// <summary>Gets/sets the Congregations.</summary>
+        /// <value>The Congregations.</value>
+        public ObservableCollection<Congregation> Congregations {
+            get => _Congregations;
+            set {
+                _Congregations = value;
+                OnPropertyChanged();
+            }
+        }
+        #endregion
+    }
+}
