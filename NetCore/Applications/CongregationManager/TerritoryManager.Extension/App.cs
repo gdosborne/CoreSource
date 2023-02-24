60745681c79f8bcde01668c888c3c263ea3eeb19
greg-osborne <greg.osborne@hp.com>
2023-02-24 11:29:01 -0600

    moved local repo to OneDrive

---
+++ NetCore/Applications/CongregationManager/TerritoryManager.Extension/App.cs
@@ -0,0 +1,25 @@
+using Common.Applicationn.Logging;
+using Common.Applicationn;
+using CongregationManager.Data;
+using System;
+using static Common.Applicationn.Logging.ApplicationLogger;
+using System.Text;
+
+namespace TerritoryManager.Extension {
+    internal static class App {
+        public static void Main(string[] args) { }
+
+        internal static ApplicationLogger logger { get; set; } = default;
+        internal static Settings AppSettings { get; set; } = default;
+        internal static DataManager DataManager { get; set; } = default;
+
+        internal static void LogMessage(string message, EntryTypes type) {
+            logger.LogMessage(new StringBuilder(message), type);
+        }
+
+        internal static Territory? AddTerritory() {
+
+            return null;
+        }
+    }
+}
